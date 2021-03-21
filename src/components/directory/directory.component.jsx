588e4d68dd51f885e59a36b0f3263ff8d94d8c10
Paul Billings <paulbillings101@yahoo.co.uk>
2021-03-21 14:02:40 +0000

    changed hats and mens pictures on homepage

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,7 +11,8 @@ class Directory extends React.Component {
       sections: [
         {
           title: "hats",
-          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+          imageUrl:
+            "https://images.unsplash.com/photo-1529958030586-3aae4ca485ff?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
           id: 1,
           linkUrl: "shop/hats",
         },
@@ -36,7 +37,8 @@ class Directory extends React.Component {
         },
         {
           title: "mens",
-          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+          imageUrl:
+            "https://images.unsplash.com/photo-1595152128156-ed2670305e8e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
           size: "large",
           id: 5,
           linkUrl: "shop/mens",
