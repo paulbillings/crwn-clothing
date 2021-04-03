b891892ac1f68fb8169725380fb772aa9ff7ac0c
Paul Billings <paulbillings101@yahoo.co.uk>
2021-04-03 14:58:45 +0100

    added button type to sign in with google button to prevent email field to ask user to fill in

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -62,7 +62,11 @@ class SignIn extends React.Component {
           />
           <div className="buttons">
             <CustomButton type="submit">Sign In</CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            <CustomButton
+              type="button"
+              onClick={signInWithGoogle}
+              isGoogleSignIn
+            >
               Sign In With{" "}
               <img
                 src="https://img.icons8.com/plasticine/100/000000/google-logo.png"
